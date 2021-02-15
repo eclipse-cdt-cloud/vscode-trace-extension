783c3e71c06851e24b2607c891b58203bf0c9a0e
Geneviève Bastien <gbastien@versatic.net>
2021-02-15 11:35:46 -0500

    Rename App.tsx to trace-viewer-container.tsx
    Signed-off-by: Geneviève Bastien <gbastien@versatic.net>

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import TraceViewerContainer from './trace-viewer-container';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <TraceViewerContainer />,
   (document.getElementById('root') as HTMLElement)
 );
