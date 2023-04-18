33d70b1d531c1ce6652496c4768febcba5be76b2
Patrick Tasse <patrick.tasse@gmail.com>
2023-04-18 10:30:41 -0400

    Configure integration tests with Playwright
    Add and configure Playwright in vscode-trace-extension package.
    
    Add extension-test.spec.ts with a few simple tests.
    
    Ignore and exclude test files and folders from build and version
    control.
    
    Partially fixes Issue #34
    
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>

---
+++ vscode-trace-extension/src/test/extension-test.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.getByRole('tab', { name: 'Welcome, preview' }).getByRole('button', { name: /Close/ }).click();
+    await page.getByRole('button', { name: 'Never' }).click();
+});
+
+test('Open Trace from Explorer', async ({ page }) => {
+    await page.getByRole('treeitem', { name: '202-bug-hunt' }).locator('a').click();
+    await page.getByRole('treeitem', { name: 'cat-kernel' }).locator('a').click({ button: 'right' });
+    await page.getByRole('menuitem', { name: 'Open with Trace Viewer' }).hover();
+    await page.getByRole('menuitem', { name: 'Open with Trace Viewer' }).click();
+    await expect(page.getByRole('tab', { name: 'cat-kernel'})).toBeVisible();
+});
+
+test('Open Trace from Trace Viewer', async ({ page }) => {
+    await page.getByRole('tab', { name: 'Trace Viewer' }).locator('a').click();
+    await page.getByRole('button', { name: 'Opened Traces Section' }).hover();
+    await page.getByRole('button', { name: 'Open Trace' }).click();
+    await page.getByRole('option', { name: '202-bug-hunt' }).locator('a').click();
+    await page.getByRole('option', { name: 'cat-kernel' }).locator('a').click();
+    await page.waitForTimeout(1000);
+    await page.getByRole('button', { name: 'OK' }).click();
+    await expect(page.getByRole('tab', { name: 'cat-kernel'})).toBeVisible();
+});
