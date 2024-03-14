68ee396eeb32eef21e0c8a0687b59dcc72732977
williamsyang-work <william.yang@ericsson.com>
2024-03-14 19:42:08 +0100

    asExternalUri for Automatic Port Forwarding
    Re-writes some code to accommodate the thenable return of asExternalUri.
    
    Signed-off-by: Will Yang <william.yang@ericsson.com>

--- vscode-trace-extension/src/utils/backend-tsp-client-provider.ts
+++ vscode-trace-extension/src/utils/backend-tsp-client-provider.ts
@@ -9,11 +9,11 @@ import { TspClientProvider } from 'vscode-trace-common/lib/client/tsp-client-pro
  *  for the Trace Extension's VSCode Backend
  */
 
-let _root = getUriRootFromUserSettings();
-let _path = getApiPathFromUserSettings();
-let _url = _root + _path;
-
-const _provider = new TspClientProvider(_url, undefined);
+let _extUri: vscode.Uri;
+let _root: string;
+let _path: string;
+let _url: string;
+let _provider: TspClientProvider;
 
 export const getTraceServerUrl = (): string => _root;
 export const getTspApiEndpoint = (): string => _path;
@@ -23,11 +23,19 @@ export const getTspClient = (): TspClient => _provider.getTspClient();
 export const getExperimentManager = (): ExperimentManager => _provider.getExperimentManager();
 export const getTraceManager = (): TraceManager => _provider.getTraceManager();
 
-export const updateTspClientUrl = (): void => {
-    _root = getUriRootFromUserSettings();
+export const updateTspClientUrl = async (): Promise<string> => {
+    _extUri = await getExternalUriFromUserSettings();
+    _root = _extUri.toString();
     _path = getApiPathFromUserSettings();
     _url = _root + _path;
-    _provider.updateTspClientUrl(_url);
+
+    if (!_provider) {
+        _provider = new TspClientProvider(_url, undefined);
+    } else {
+        _provider.updateTspClientUrl(_url);
+    }
+
+    return _url;
 };
 
 export const addTspClientChangeListener = (listenerFunction: (tspClient: TspClient) => void): void => {
@@ -60,10 +68,12 @@ export async function updateNoExperimentsContext(): Promise<void> {
     return;
 }
 
-function getUriRootFromUserSettings(): string {
+async function getExternalUriFromUserSettings(): Promise<vscode.Uri> {
     const tsConfig = vscode.workspace.getConfiguration('trace-compass.traceserver');
     const traceServerUrl: string = tsConfig.get<string>('url') || 'http://localhost:8080';
-    return traceServerUrl.endsWith('/') ? traceServerUrl : traceServerUrl + '/';
+    const url = traceServerUrl.endsWith('/') ? traceServerUrl : traceServerUrl + '/';
+    const baseUri = vscode.Uri.parse(url);
+    return vscode.env.asExternalUri(baseUri);
 }
 
 function getApiPathFromUserSettings(): string {
