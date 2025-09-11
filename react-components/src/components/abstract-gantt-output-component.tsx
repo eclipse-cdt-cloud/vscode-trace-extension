043a0f94807dc669505bfb60bbda7de072cd2213
Bernd Hufmann <bernd.hufmann@ericsson.com>
2025-09-11 16:03:00 -0400

    Squashed 'local-libs/traceviewer-libs/' changes from 5a175c2..e2729f9
    e2729f9 Merge pull request #4
    a9835dc Merge commit '3ed90349345e6e3b96d5848f4d727bd8373a254f' into update-subtree
    0c2bddb [traceviewer-libs] correct git subtree push commands
    580f22f [traceviewer-base] explicitly request recent prettier dependency
    196ae3a Adapt after adding local traceviewer-* libs as subtrees
    62a1f66 Check if style.parentKey field is available before using it.
    ba05359 adjust misc configurations
    
    git-subtree-dir: local-libs/traceviewer-libs
    git-subtree-split: e2729f9cfd337009d56af3dfb43c248fab9a0663

--- react-components/src/components/abstract-gantt-output-component.tsx
+++ react-components/src/components/abstract-gantt-output-component.tsx
@@ -537,8 +537,8 @@ export abstract class AbstractGanttOutputComponent<
         return this.state.markerCategoryEntries.length <= 1
             ? 0
             : this.state.collapsedMarkerNodes.length
-            ? rowHeight
-            : this.state.markerCategoryEntries.length * rowHeight + scrollbarHeight;
+              ? rowHeight
+              : this.state.markerCategoryEntries.length * rowHeight + scrollbarHeight;
     }
 
     abstract renderTree(): React.ReactNode;
@@ -1381,21 +1381,26 @@ export abstract class AbstractGanttOutputComponent<
             }
 
             const stateStyle = outputStyle as OutputElementStyle;
-            const elementStyle = styles[stateStyle.parentKey];
-            if (elementStyle) {
-                return {
-                    color: parseInt(elementStyle.color, 16),
-                    height: this.props.style.rowHeight * elementStyle.height,
-                    borderWidth: state.selected ? 2 : 0,
-                    borderColor: 0xeef20c
-                };
+            let parentKey = 'no-parent';
+            if (stateStyle.parentKey) {
+                parentKey = stateStyle.parentKey;
+                const elementStyle = styles[parentKey];
+                if (elementStyle) {
+                    return {
+                        color: parseInt(elementStyle.color, 16),
+                        height: this.props.style.rowHeight * elementStyle.height,
+                        borderWidth: state.selected ? 2 : 0,
+                        borderColor: 0xeef20c
+                    };
+                }
+                style = this.styleMap.get(parentKey);
             }
 
-            style = this.styleMap.get(stateStyle.parentKey);
             if (style === undefined) {
-                style = backupStyles[(Math.abs(hash(stateStyle.parentKey)) as number) % backupStyles.length];
-                this.styleMap.set(stateStyle.parentKey, style);
+                style = backupStyles[Math.abs(hash(parentKey)) % backupStyles.length];
+                this.styleMap.set(parentKey, style);
             }
+
             return {
                 color: style.color,
                 height: style.height,
